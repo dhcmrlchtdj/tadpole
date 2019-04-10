b622f2874cd65ae510d3e0bc0c338d2d474886e3
h11 <nirisix@gmail.com>
2019-04-10 21:46:30 +0800

    update: scanner

--- src/watScanner.ml
+++ src/watScanner.ml
@@ -24,76 +24,35 @@ let hex_of_char_list (chars : char list) : int =
     aux 0 chars
 
 
-let int_of_char_list (chars : char list) : int =
-    let rec aux (acc : int) = function
-        | [] -> acc
-        | '0' :: t -> aux ((acc * 10) + 0) t
-        | '1' :: t -> aux ((acc * 10) + 1) t
-        | '2' :: t -> aux ((acc * 10) + 2) t
-        | '3' :: t -> aux ((acc * 10) + 3) t
-        | '4' :: t -> aux ((acc * 10) + 4) t
-        | '5' :: t -> aux ((acc * 10) + 5) t
-        | '6' :: t -> aux ((acc * 10) + 6) t
-        | '7' :: t -> aux ((acc * 10) + 7) t
-        | '8' :: t -> aux ((acc * 10) + 8) t
-        | '9' :: t -> aux ((acc * 10) + 9) t
-        | _ -> failwith "never"
-    in
-    aux 0 chars
-
-
-let frac_hex_of_char_list (chars : char list) : float =
-    let rec aux (acc : float) = function
-        | [] -> acc
-        | '0' :: t -> aux ((acc +. 0.) /. 16.) t
-        | '1' :: t -> aux ((acc +. 1.) /. 16.) t
-        | '2' :: t -> aux ((acc +. 2.) /. 16.) t
-        | '3' :: t -> aux ((acc +. 3.) /. 16.) t
-        | '4' :: t -> aux ((acc +. 4.) /. 16.) t
-        | '5' :: t -> aux ((acc +. 5.) /. 16.) t
-        | '6' :: t -> aux ((acc +. 6.) /. 16.) t
-        | '7' :: t -> aux ((acc +. 7.) /. 16.) t
-        | '8' :: t -> aux ((acc +. 8.) /. 16.) t
-        | '9' :: t -> aux ((acc +. 9.) /. 16.) t
-        | 'a' :: t | 'A' :: t -> aux ((acc +. 10.) /. 16.) t
-        | 'b' :: t | 'B' :: t -> aux ((acc +. 11.) /. 16.) t
-        | 'c' :: t | 'C' :: t -> aux ((acc +. 12.) /. 16.) t
-        | 'd' :: t | 'D' :: t -> aux ((acc +. 13.) /. 16.) t
-        | 'e' :: t | 'E' :: t -> aux ((acc +. 14.) /. 16.) t
-        | 'f' :: t | 'F' :: t -> aux ((acc +. 15.) /. 16.) t
-        | _ -> failwith "never"
-    in
-    aux 0. chars
-
-
-let frac_int_of_char_list (chars : char list) : float =
-    let rec aux (acc : float) = function
-        | [] -> acc
-        | '0' :: t -> aux ((acc +. 0.) /. 10.) t
-        | '1' :: t -> aux ((acc +. 1.) /. 10.) t
-        | '2' :: t -> aux ((acc +. 2.) /. 10.) t
-        | '3' :: t -> aux ((acc +. 3.) /. 10.) t
-        | '4' :: t -> aux ((acc +. 4.) /. 10.) t
-        | '5' :: t -> aux ((acc +. 5.) /. 10.) t
-        | '6' :: t -> aux ((acc +. 6.) /. 10.) t
-        | '7' :: t -> aux ((acc +. 7.) /. 10.) t
-        | '8' :: t -> aux ((acc +. 8.) /. 10.) t
-        | '9' :: t -> aux ((acc +. 9.) /. 10.) t
-        | _ -> failwith "never"
-    in
-    aux 0. chars
-
-
-let str_of_rev_list (chars : char list) : string =
+let str_of_rev_char_list (chars : char list) : string =
     chars |> List.rev |> String.of_list
+
+
+let num_of_string (s : string) : Token.t option =
+    match Int.of_string s with
+        | Some n -> Some (Token.INT n)
+        | None ->
+            (try
+                let n = Float.of_string_exn s in
+                Some (Token.FLOAT n)
+            with
+                | _ -> None)
 
 
 let is_digit = function '0' .. '9' -> true | _ -> false
 
 let is_hexdigit = function
     | '0' .. '9' -> true
-    | 'a' .. 'z' -> true
-    | 'A' .. 'Z' -> true
+    | 'a' .. 'f' -> true
+    | 'A' .. 'F' -> true
+    | _ -> false
+
+
+let is_num_char = function
+    | '0' .. '9' -> true
+    | 'a' .. 'f' -> true
+    | 'A' .. 'F' -> true
+    | '.' | '+' | '-' | 'p' | 'P' | 'x' -> true
     | _ -> false
 
 
@@ -109,11 +68,11 @@ let is_idchar = function
 
 let is_space = function ' ' | '\t' | '\n' | '\r' -> true | _ -> false
 
-let scan (src : string) : WatToken.t list =
-    let open WatToken in
-    let rec scan_ (acc : WatToken.t list) (t : char list) =
+let scan (src : string) : Token.t list =
+    let open Token in
+    let rec scan_all_token (acc : Token.t list) (t : char list) =
         match scan_token t with
-            | Ok (Some tok, tt) -> scan_ (tok :: acc) tt
+            | Ok (Some tok, tt) -> scan_all_token (tok :: acc) tt
             | Ok (None, []) -> List.rev acc
             | Ok (None, _) -> failwith "[scan_] never"
             | Error s -> failwith s
@@ -121,23 +80,11 @@ let scan (src : string) : WatToken.t list =
         | '_' :: h :: t when is_hexdigit h -> scan_hexdigit (h :: acc) t
         | h :: t when is_hexdigit h -> scan_hexdigit (h :: acc) t
         | t -> Ok (Some (acc |> List.rev |> hex_of_char_list), t)
-    and scan_digit (acc : char list) = function
-        | '_' :: h :: t when is_digit h -> scan_digit (h :: acc) t
-        | h :: t when is_digit h -> scan_digit (h :: acc) t
-        | t -> Ok (Some (acc |> List.rev |> int_of_char_list), t)
-    and scan_hexdigit_frac (acc : char list) = function
-        | '_' :: h :: t when is_hexdigit h -> scan_hexdigit_frac (h :: acc) t
-        | h :: t when is_hexdigit h -> scan_hexdigit_frac (h :: acc) t
-        | t -> Ok (Some (acc |> frac_hex_of_char_list), t)
-    and scan_digit_frac (acc : char list) = function
-        | '_' :: h :: t when is_digit h -> scan_digit_frac (h :: acc) t
-        | h :: t when is_digit h -> scan_digit_frac (h :: acc) t
-        | t -> Ok (Some (acc |> frac_int_of_char_list), t)
     and scan_line_comment = function
         | ';' :: ';' :: t ->
             let rec aux (acc : char list) = function
-                | [] -> Ok (Some (COMMENT (acc |> str_of_rev_list)), [])
-                | '\n' :: t -> Ok (Some (COMMENT (acc |> str_of_rev_list)), t)
+                | [] -> Ok (Some (COMMENT (acc |> str_of_rev_char_list)), [])
+                | '\n' :: t -> Ok (Some (COMMENT (acc |> str_of_rev_char_list)), t)
                 | h :: t -> aux (h :: acc) t
             in
             aux [] t
@@ -150,7 +97,7 @@ let scan (src : string) : WatToken.t list =
                 if lv = 0
                 then (
                     let acc2 = ')' :: ';' :: acc in
-                    Ok (Some (COMMENT (acc2 |> str_of_rev_list)), t))
+                    Ok (Some (COMMENT (acc2 |> str_of_rev_char_list)), t))
                 else aux lv acc t
             | h :: t -> aux nest_level (h :: acc) t
             | [] -> failwith "never"
@@ -159,16 +106,16 @@ let scan (src : string) : WatToken.t list =
     and scan_id (acc : char list) = function
         | h :: t when is_idchar h -> scan_id (h :: acc) t
         | t ->
-            let r = acc |> str_of_rev_list in
+            let r = acc |> str_of_rev_char_list in
             if String.length r = 1
             then Ok (Some (RESERVED r), t)
             else Ok (Some (ID r), t)
     and scan_keyword (acc : char list) = function
         | h :: t when is_idchar h -> scan_keyword (h :: acc) t
-        | t -> Ok (Some (KEYWORD (acc |> str_of_rev_list)), t)
+        | t -> Ok (Some (KEYWORD (acc |> str_of_rev_char_list)), t)
     and scan_reserved (acc : char list) = function
         | h :: t when is_idchar h -> scan_reserved (h :: acc) t
-        | t -> Ok (Some (RESERVED (acc |> str_of_rev_list)), t)
+        | t -> Ok (Some (RESERVED (acc |> str_of_rev_char_list)), t)
     and scan_string (acc : char list) = function
         (* TODO https://webassembly.github.io/spec/core/text/values.html#strings *)
         | '\\' :: n :: m :: t when is_hexdigit n && is_hexdigit m ->
@@ -190,54 +137,44 @@ let scan (src : string) : WatToken.t list =
         | '\\' :: '\'' :: t -> scan_string ('\'' :: acc) t
         | '\\' :: '\\' :: t -> scan_string ('\\' :: acc) t
         | '\\' :: _ -> failwith "[scan_string] invalid slash"
-        | '"' :: t -> Ok (Some (STRING (acc |> str_of_rev_list)), t)
+        | '"' :: t -> Ok (Some (STRING (acc |> str_of_rev_char_list)), t)
         | h :: t -> scan_string (h :: acc) t
         | [] -> failwith "[scan_string] unexpected eof"
-    and scan_num = function
-        | '0' :: 'x' :: t -> hex_scan_num t
-        | t -> decimal_scan_num t
-    and hex_scan_num = function _ -> failwith "TODO"
-    and decimal_scan_num t =
-        match scan_digit [] t with
-            | Ok (Some n, t2) ->
-                let n2 = float_of_int n in
-                (match t2 with
-                    | '.' :: 'E' :: t3 | '.' :: 'e' :: t3 | 'E' :: t3 | 'e' :: t3 ->
-                        decimal_scan_float_e n2 t3
-                    | '.' :: t3 -> decimal_scan_float_frac n2 t3
-                    | _ -> Ok (Some (INT n), t2))
-            | Ok _ -> failwith "never"
-            | Error e -> Error e
-    and decimal_scan_float_frac n t =
-        match scan_digit_frac [] t with
-            | Ok (Some e, t2) ->
-                let n2 = n +. e in
-                (match t2 with
-                    | 'E' :: t3 | 'e' :: t3 -> decimal_scan_float_e n2 t3
-                    | _ -> Ok (Some (FLOAT n2), t2))
-            | Ok _ -> failwith "never"
-            | Error e -> Error e
-    and decimal_scan_float_e n t =
-        let (sign, t2) =
-            match t with '-' :: t2 -> ('-', t2) | '+' :: t2 | t2 -> ('+', t2)
+    and scan_num (t : char list) =
+        let rec aux (acc : char list) = function
+            | '_' :: t -> aux acc t
+            | h :: t when is_num_char h -> aux (h :: acc) t
+            | t ->
+                let s = str_of_rev_char_list acc in
+                (match num_of_string s with
+                    | Some n -> Ok (Some n, t)
+                    | None -> Ok (Some (RESERVED s), t))
         in
-        match scan_digit [] t2 with
-            | Ok (Some e, t3) ->
-                let e2 = if Char.equal sign '+' then e else -e in
-                let e3 = Stdlib.Float.pow 10. (float_of_int e2) in
-                let n2 = n *. e3 in
-                Ok (Some (FLOAT n2), t3)
-            | Ok _ -> failwith "never"
-            | Error e -> Error e
+        let (acc, tt) =
+            match t with
+                | '+' :: tt -> ([ '+' ], tt)
+                | '-' :: tt -> ([ '-' ], tt)
+                | tt -> ([ '+' ], tt)
+        in
+        aux acc tt
     and scan_token = function
         | [] -> Ok (None, [])
+        (* remove space *)
         | h :: t when is_space h -> scan_token t
+        (* comment *)
         | '(' :: ';' :: _ as t -> scan_block_comment t
         | ';' :: ';' :: _ as t -> scan_line_comment t
+        (* parenthesis *)
         | '(' :: t -> Ok (Some LEFT_PAREN, t)
         | ')' :: t -> Ok (Some RIGHT_PAREN, t)
+        (* id or reserved *)
+        | '$' :: t -> scan_id [ '$' ] t
+        (* string *)
         | '"' :: t -> scan_string [] t
-        | '$' :: _ as t -> scan_id [] t
+        (* int or float *)
+        | ('+' :: h :: _ | '-' :: h :: _) as t when is_digit h -> scan_num t
+        | h :: _ as t when is_digit h -> scan_num t
+        (* keyword or float *)
         | 'a' .. 'z' :: _ as t ->
             (match scan_keyword [] t with
                 | Ok (Some (KEYWORD k), tt) when String.equal k "inf" ->
@@ -245,20 +182,8 @@ let scan (src : string) : WatToken.t list =
                 | Ok (Some (KEYWORD k), tt) when String.equal k "nan" ->
                     Ok (Some (FLOAT nan), tt)
                 | s -> s)
-        | '+' :: t ->
-            (match scan_num t with
-                | Ok (None, tt) -> Ok (Some (RESERVED "+"), tt)
-                | Ok (s, tt) -> Ok (s, tt)
-                | Error s -> failwith s)
-        | '-' :: t ->
-            (match scan_num t with
-                | Ok (None, tt) -> Ok (Some (RESERVED "-"), tt)
-                | Ok (Some (INT n), tt) -> Ok (Some (INT (-n)), tt)
-                | Ok (Some (FLOAT n), tt) -> Ok (Some (FLOAT (-.n)), tt)
-                | Ok _ -> failwith "never"
-                | Error s -> failwith s)
-        | h :: _ as t when is_digit h -> scan_num t
+        (* reserved *)
         | h :: t when is_idchar h -> scan_reserved [ h ] t
         | _ -> failwith "never"
     in
-    scan_ [] (String.to_list src)
+    scan_all_token [] (String.to_list src)
