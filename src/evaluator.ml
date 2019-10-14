581ce4189040a66a1004d8949d16d4e78500794c
h11 <nirisix@gmail.com>
2019-10-14 17:26:17 +0800

    update: env functor

--- src/evaluator.ml
+++ src/evaluator.ml
@@ -1,7 +1,24 @@
-type env = int
+open! Containers
+module D = Datum
 
-type value = string
+(* *** *)
 
-let to_string (v : value) : string = v
+type value = TestValue
 
-let eval (_ds : Datum.t list) : value = ""
+let to_string (_v : value) : string = ""
+
+(* *** *)
+
+module E = Env.Make (struct
+        type t = value
+    end)
+
+(* *** *)
+
+let evaluate (_ds : D.t list) (env : E.t) : value =
+    let _ = E.set env "a" TestValue in
+    let v = E.get env "a" in
+    Option.get_exn v
+
+
+let eval (ds : D.t list) : value = evaluate ds (E.create ())
