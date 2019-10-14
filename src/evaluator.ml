0a7d8a2163c7bc116ff0cba35a88c59c2425e535
h11 <nirisix@gmail.com>
2019-10-14 17:54:53 +0800

    wip

--- src/evaluator.ml
+++ src/evaluator.ml
@@ -3,7 +3,9 @@ module D = Datum
 
 (* *** *)
 
-type value = TestValue
+type value =
+    | TestValue
+    | NIL
 
 let to_string (_v : value) : string = ""
 
@@ -13,12 +15,16 @@ module E = Env.Make (struct
         type t = value
     end)
 
+let env = E.create ()
+
 (* *** *)
 
-let evaluate (_ds : D.t list) (env : E.t) : value =
+let evaluate (_datum : D.t) : value =
     let _ = E.set env "a" TestValue in
     let v = E.get env "a" in
     Option.get_exn v
 
 
-let eval (ds : D.t list) : value = evaluate ds (E.create ())
+let eval (datums : D.t list) : value =
+    let f _prev datum = evaluate datum in
+    List.fold_left f NIL datums
