a7eacfbed8b7f76dcd4ced1c1834fa384e12e67d
h11 <nirisix@gmail.com>
2019-11-18 17:36:23 +0800

    update(jsoo): use uint8array for wasm

--- jsoo/jsoo.ml
+++ jsoo/jsoo.ml
@@ -2,31 +2,33 @@ open! Containers
 open Js_of_ocaml
 open Tadpole
 
-let wrap parse s =
-  let src = Js.to_string s in
+let of_jsString = Js.to_string
+
+let to_jsString = Js.string
+
+let of_jsUint8Array = Typed_array.String.of_uint8Array
+
+let to_jsUint8Array (s : string) : Typed_array.uint8Array Js.t =
+  let arr =
+    s |> String.to_list |> List.map Char.code |> Array.of_list |> Js.array
+  in
+  new%js Typed_array.uint8Array_fromArray arr
+
+let wrap f src =
+  let m = Result.get_or_failwith (f src) in
   object%js
-    method toWASM =
-      src
-      |> parse
-      |> Result.get_or_failwith
-      |> WasmPrinter.to_string
-      |> Js.string
+    method toWASM = to_jsUint8Array (WasmPrinter.to_string m)
 
     method toWAT =
-      src
-      |> parse
-      |> Result.get_or_failwith
-      |> WatPrinter.to_string
-      |> WatPrettyPrinter.pretty
-      |> Js.string
+      to_jsString (WatPrettyPrinter.pretty (WatPrinter.to_string m))
   end
 
 let () =
   let obj =
     object%js
-      method _WAT src = wrap WatParser.parse src
+      method _WAT = wrap (fun s -> WatParser.parse (of_jsString s))
 
-      method _WASM src = wrap WasmParser.parse src
+      method _WASM = wrap (fun s -> WasmParser.parse (of_jsUint8Array s))
     end
   in
   Js.export_all obj
