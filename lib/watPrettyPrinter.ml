140b4b841da58934c9b0d6cc26cad0d01401d3e6
h11 <nirisix@gmail.com>
2019-11-18 13:23:52 +0800

    update: pretty printer

--- lib/watPrettyPrinter.ml
+++ lib/watPrettyPrinter.ml
@@ -4,81 +4,77 @@ module D = WatDatum
 let sprintf = Printf.sprintf
 
 module Document = struct
-  type doc =
+  type t =
     | Newline of int
     | Text of string
-    | Group of doc
-    | Concat of doc list
+    | Group of t list
 
-  let max_width = 80
-
-  let to_string =
+  let to_string max_width indent_size =
     let rec aux used = function
-      | Newline i -> sprintf "\n%s" (String.repeat " " i)
+      | Newline i -> sprintf "\n%s" (String.repeat " " (i * indent_size))
       | Text s -> s
-      | Group d ->
-        let s = to_string_group d in
+      | Group ds as g ->
+        let s = oneline g in
         let len = String.length s in
-        if len + used <= max_width then s else aux used d
-      | Concat ds ->
-        let (_, dss) =
-          List.fold_left
-            (fun (w, prev) next ->
-              let s = aux w next in
-              let r =
-                match next with
-                | Newline i -> i
-                | _ -> w + String.length s
-              in
-              (r, s :: prev))
-            (used, [])
-            ds
-        in
-        dss |> List.rev |> String.concat ""
-    and to_string_group = function
+        if len + used <= max_width then s else multiline used ds
+    and oneline = function
       | Newline _ -> " "
       | Text s -> s
-      | Group d -> to_string_group d
-      | Concat ds -> List.map to_string_group ds |> String.concat ""
+      | Group ds -> ds |> List.map oneline |> String.concat ""
+    and multiline used ds =
+      let (_, dss) =
+        let f ((prev_used, docs) : int * string list) (doc : t) =
+          let s = aux prev_used doc in
+          let next_used =
+            match doc with
+            | Newline i -> i * indent_size
+            | _ -> prev_used + String.length s
+          in
+          (next_used, s :: docs)
+        in
+        List.fold_left f (used, []) ds
+      in
+      dss |> List.rev |> String.concat ""
     in
     aux 0
 
   let of_datum =
+    let rec add_indent = function
+      | Newline i -> Newline (i + 1)
+      | Text s -> Text s
+      | Group ds -> Group (List.map add_indent ds)
+    in
+    let left_paren = [ Text "(" ] in
+    let right_paren = [ Text ")" ] in
+    let group docs =
+      let sub = List.map add_indent docs in
+      List.flatten [ left_paren; sub; right_paren ]
+    in
     let rec aux = function
       | D.NUM s -> Text s
       | D.STRING s -> Text (sprintf "%S" s)
       | D.ID s -> Text s
       | D.KEYWORD s -> Text s
       | D.RESERVED s -> Text s
-      | D.LIST t ->
-        let sub =
-          let doc = List.map aux t in
-          let len = List.length doc - 1 in
-          doc
-          |> List.mapi (fun i x -> if i = len then [ x ] else [ x; Newline 0 ])
+      | D.LIST ds ->
+        let docs =
+          ds
+          |> List.mapi (fun i x ->
+                 let x = aux x in
+                 if i = 0 then [ x ] else [ Newline 0; x ])
           |> List.flatten
-          |> List.map add_indent
-          |> fun x -> Concat x
+          |> group
         in
-        let doc = Concat [ Text "("; sub; Text ")" ] |> simplify in
-        Group (Concat doc)
-    and add_indent = function
-      | Newline i -> Newline (i + 4)
-      | Text s -> Text s
-      | Group d -> Group (add_indent d)
-      | Concat ds -> Concat (List.map add_indent ds)
-    and simplify = function
-      | Concat doc -> doc |> List.map simplify |> List.flatten
-      | x -> [ x ]
+        Group docs
     in
     aux
 end
 
-let pretty (src : string) : string =
+let pretty ?(max_width = 80) ?(indent_size = 4) (src : string) : string =
   src
   |> WatScanner.scan
   |> Result.get_or_failwith
   |> WatDatum.of_tokens
   |> Result.get_or_failwith
   |> Document.of_datum
-  |> Document.to_string
+  |> Document.to_string max_width indent_size
